bf04a95a201903eb0ef5a968d7e54454244cb1b8
JuanDavidAvilaRaveloCampus <juanavila8856@gmail.com>
2023-02-06 21:20:08 -0500

    Prueba Con Python

---
+++ Python-1.py
@@ -0,0 +1,7 @@
+nombre = "Juan David Avila Ravelo"
+edad = "19"
+hobbies = [
+    "Leer", "Escuchar Música", "Dibujar Anatomía Humana", "Salir a comer helado", "Ver pelis", "Programar", "Bailar," "Cocinar", "Chismosear con mi hermana", "Jugar con la perra de mi hernana",
+]
+
+print(f"Hola! Mi nombre es {nombre} \nTengo {edad} de edad \nMe encanta {hobbies[2]}")
