e2d5aa9fe78fa274cb31ebe85cd44e30f41bc519
JuanDavidAvilaRaveloCampus <juanavila8856@gmail.com>
2023-02-20 02:07:05 -0500

    Python

--- Python/Funtions Solution/solution#1.py
+++ Python/Funtions Solution/solution#1.py
@@ -4,34 +4,49 @@
 # un programa que cuente con el siguiente menú como panel de
 # control:
 
-menuPrincipal = int(input("Menu: \n 1. CREAR GRUPO ARTEMIS:\n 2. CREAR GRUPO SPUTNIK:\n"))
-crearGrupoArtemis =  int(input(f"\n 1.1 LISTAR CAMPERS DE ARTEMIS\n 1.2 AGREGAR CAMPERS A ARTEMIS \n 1.3 ELIMINAR CAMPERRS DE ARTEMIS \n 1.4 ORDENAR ALFABÉTICAMENTE EN LISTA DE ARTEMIS \n 1.5 BUSCAR CAMPER EN LISTA DE ARTEMIS\n"))
+print(f'\n------------------------MENU------------------------\n1.   CREAR GRUPO ARTEMIS:\n1.1  LISTAR CAMPERS DE ARTEMIS\n1.2  AGREGAR CAMPERS A ARTEMIS\n1.3  ELIMINAR CAMPERS DE ARTERMIS\n1.4  ORDENAR ALFABÉTICAMENTE EN UNA LISTA DE ARTEMIS\n1.5  BUSCAR CAMPER EN LISTA DE ARTEMIS\n2    CREAR GRUPO SPUTNIK:\n2.1  LISTAR CAMPERS DE CAMPERS DE SPUTNIK:\n2.2  AGREGAR CAMPSER A SPUTNIK\n2.3  ELIMINAR CAMPERS DE SPUTNIK\n2.4  ORDENAR ALFABETICAMENTE EN LISTA DE SPUTNIK\n2.5  BUSCAR CAMPER EN LISTA DE SPUTNIK')
 
-while menuPrincipal != 0:
-    if menuPrincipal == 1:
-        print(f"\n 1.1 LISTAR CAMPERS DE ARTEMIS\n 1.2 AGREGAR CAMPERS A ARTEMIS \n 1.3 ELIMINAR CAMPERRS DE ARTEMIS \n 1.4 ORDENAR ALFABÉTICAMENTE EN LISTA DE ARTEMIS \n 1.5 BUSCAR CAMPER EN LISTA DE ARTEMIS\n")
-        if crearGrupoArtemis == 1:
-            print(f"LISTADO CAMPERS ARTEMIS\n")
-        if crearGrupoArtemis == 2:
-            print(f"AGREGAR CAMPER AL GRUPO ARTEMIS\n")
-        if crearGrupoArtemis == 3:
-            print(f"ELIMINAR CAMPER ARTEMIS\n")
-        if crearGrupoArtemis == 4:
-            print(f"ORDENAR ALFABÉTICAMENTE EN LISTA ARTEMIS\n")
-        if crearGrupoArtemis == 5:
-            print(f"BUSCAR EN CAMPERS ARTEMIS\n")
-    elif menuPrincipal == 2:
-        print(f"\n 2.1 LISTAR CAMPERS SPUTNIK \n 2.2 AGREGAR CAMPERS A SPUTNIK \n 2.3 ELIMINAR CAMPERS SPUTNIK \n 2.4 ORDENAR ALFABETICAMENTE EN LISTA DE SPUTNIK \n 2.5 BUSCAR CAMPER EN LA LISTA DE SPUTNIK\n")
-        if crearGrupoArtemis == 1:
-            print(f"LISTADO CAMPERS SPUTNIK\n")
-        if crearGrupoArtemis == 2:
-            print(f"AGREGAR CAMPER AL GRUPO SPUTNIK\n")
-        if crearGrupoArtemis == 3:
-            print(f"ELIMINAR CAMPER SPUTNIK\n")
-        if crearGrupoArtemis == 4:
-            print(f"ORDENAR ALFABÉTICAMENTE EN LISTA SPUTNIK\n")
-        if crearGrupoArtemis == 5:
-            print(f"BUSCAR EN CAMPERS SPUTNIK\n")
-    else:
-        print(f"Por favor digita un numero disponible")
-    menuPrincipal = int(input("Menu: \n 1. CREAR GRUPO ARTEMIS:\n 2. CREAR GRUPO SPUTNIK:\n"))        
+crear_grupo = int(input('Dijite la opción: '))
+
+
+proseguir = 0
+listar = 0
+agregar = 0
+eliminar = 0
+ordenar_alpha = 0
+buscar = 0
+
+grupo_artemis = []
+grupo_sputnik = []
+
+
+
+
+
+if crear_grupo == 1:
+    print(f'\nSe ha creado el grupo Artemis..')
+    print(f'Si deasea listar el grupo Artemis, presione 1.\nSi desea agregar campers, presione 2.\nSi desea eliminar un camper, presione 3.\nSi desea ordenarlos presione 4 y si desea buscar un camper presione 5')
+    proseguir = int(input('Dijite su respuesta: '))
+
+    #LISTAR
+
+
+    #if proseguir == 1:
+    #AGREGAR
+    while proseguir == 2:
+
+        nombreUsuario = input('\nAgregue el nombre del camper: ')
+        grupo_artemis.append(nombreUsuario)
+
+        print('\nSi desea agregar otro camper a la lista dijite 2.\nSi desea salir, dijite 0')
+        proseguir = int(input(f'Dijite la opción: '))
+
+    #ELIMINAR
+    #ORDENAR
+    #BUSCAR
+
+    #ToDO DEBE ESTAR DENTRO DE UN WHILE,
+    #PARA CUANDO QUERRAMOS PREGUNTAR SI QUIERE SALIR, 
+    #QUE PRESIONE 0 Y SI QUIERE ELEJIR OTRA OPCIÓN,
+    #QUE PRESIONE 1 Y SE REINICIE EL BUCLE
+
--- Python/Solution/Introduction/Solution#10.py
+++ Python/Solution/Introduction/Solution#10.py
@@ -5,4 +5,26 @@
 # - Mes de consumo - Valor kw
 # -Total kw consumido en el mes - estrato
 
+estrato = int(input('\ningrese su estrato: '))
+valor_kw = 100
+total_wk_consumido = valor_kw * 30
+mes_de_consumo = valor_kw * total_wk_consumido
 
+if estrato == 1:
+    valor_estrato = 90*mes_de_consumo / 100
+
+elif estrato == 2:
+    valor_estrato = 70*mes_de_consumo / 100
+
+elif estrato == 3:
+    valor_estrato = 40*mes_de_consumo / 100
+
+elif estrato == 4:
+    valor_estrato = 20*mes_de_consumo / 100
+
+else:
+    valor_estrato = 0
+    
+pagar = mes_de_consumo - valor_estrato
+
+print(f'su total a pagar es de: {pagar}')
